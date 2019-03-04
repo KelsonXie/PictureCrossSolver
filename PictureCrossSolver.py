570bc1ec809a9b7775a1b9f539d59da36f0758cd
KelsonXie <kelson.xie99@gmail.com>
2019-03-04 18:43:46 -0500

    made some adjustments

--- PictureCrossSolver.py
+++ PictureCrossSolver.py
@@ -3,16 +3,25 @@ class PictureCross():
     def __init__(self, height, width, row_list, col_list):
         self.height = height
         self.width = width
-        self.r_list = height_list
-        self.c_list = width_list
+        self.r_list = row_list
+        self.c_list = col_list
+        self.solved_rows = []
+        self.solved_cols = []
         self.grid = []
-        for i in range(0, height-1):
+        for i in range(0, height):
             self.grid.append([])
-            for j in range(0, width-1):
-                self.grid[i].append(0)
+            self.solved_rows.append("0")
+            self.solved_cols.append("0")
+            for j in range(0, width):
+                self.grid[i].append("X")
+
 
     def __str__(self):
         print(self.grid)
+        return("")
 
-a = PictureCross(10, 10, [1], [1])
-print(a)
+    def solve(self):
+        for i in range(0, )
+        solved = False;
+        while(not solved):
+            
